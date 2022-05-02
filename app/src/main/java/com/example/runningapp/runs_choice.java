301e38333c234cc7ab89c76246d7b1e0b104236f
maximkes <max_pechin@mail.ru>
2022-05-02 22:50:22 +0300

    Added ScrollView on runs_choice

--- app/src/main/java/com/example/runningapp/runs_choice.java
+++ app/src/main/java/com/example/runningapp/runs_choice.java
@@ -6,7 +6,8 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RelativeLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.runningapp.db.AppDatabase;
@@ -19,14 +20,14 @@ public class runs_choice extends Activity {
     ArrayList<Integer> Timings; // все продолжительности забегов
     ArrayList<Double> Distances; // все расстояния забегов
 
-    RelativeLayout relativeLayout;
+    LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_runs_choice);
 
-        relativeLayout = findViewById(R.id.RelativeLayout);
+        linearLayout = findViewById(R.id.LinearLayoutInCsV);
 
         IDs = new ArrayList<Integer>();
         Timings = new ArrayList<Integer>();
@@ -75,7 +76,7 @@ public class runs_choice extends Activity {
     void OnClick(View v)  //  Обработчик кнопки  "начать"
     {
         RunRecord old_run;
-        int chosen_id = IDs.get(((int) v.getId() - 3) / 5); // находит нужный id
+        int chosen_id = IDs.get((int)v.getTag()); // находит нужный id
 
         Intent intent = new Intent(this, TimeStepCounter.class);
 
@@ -94,7 +95,7 @@ public class runs_choice extends Activity {
 
     void OnClickDelete(View v)  //  Обработчик кнопки  "Удалить"
     {
-        int chosen_id = IDs.get(((int) v.getId() - 4) / 5);// находит нужный id
+        int chosen_id = IDs.get((int)v.getTag());// находит нужный id
         AppDatabase db = AppDatabase.getDbInstance(this.getApplicationContext());
         db.userDao().DeleteGivenTrack(chosen_id); //Удаляет запись
         Intent intent = new Intent(this, runs_choice.class); // перезапускает страницу
@@ -108,6 +109,7 @@ public class runs_choice extends Activity {
         intent.putExtra("old_run", old_run);
         startActivity(intent);
     }
+
     void UpdatePage()
     {
         GetIDs();
@@ -115,85 +117,54 @@ public class runs_choice extends Activity {
         GetDistances();
         for(int i=0; i<IDs.size()+1; i++)
         {
-            TextView tv_ID=new TextView(runs_choice.this, null);
-            TextView tv_Distance=new TextView(runs_choice.this, null);
-            TextView tv_Time=new TextView(runs_choice.this, null);
-            Button btn_Choose = new Button(runs_choice.this, null);
-            Button btn_Remove = new Button(runs_choice.this, null);
-
+            LinearLayout T_LL, B_LL;
             if(i<IDs.size()) {
                 String time = String.format("%d:%02d:%02d", Timings.get(i) / 3600, (Timings.get(i) % 3600) / 60, (Timings.get(i)) % 60);
 
-                btn_Remove.setText("Remove");
-                tv_ID.setText((String) (IDs.get(i) + ""));
-                tv_Distance.setText((String) (Distances.get(i) + ""));
-                tv_Time.setText(time);
-                btn_Choose.setText("Начать");
+                T_LL =  (LinearLayout) getLayoutInflater().inflate(R.layout.rc_single_run_text, null);
+                B_LL = (LinearLayout) getLayoutInflater().inflate(R.layout.rc_single_run_buttons, null);
 
-                tv_ID.setId(5 * i);
-                tv_Distance.setId(5 * i + 1);
-                tv_Time.setId(5 * i + 2);
-                btn_Choose.setId(5 * i + 3);
-                btn_Remove.setId(5*i+4);
+                Button btn_Choose = (Button) B_LL.getChildAt(0);
+                Button btn_Delete = (Button) B_LL.getChildAt(1);
 
-                tv_ID.setTextSize(30);
-
-
-                tv_ID.setY(150 * i);
-                tv_ID.setX(0);
-
-                tv_Distance.setY(150 * i);
-                tv_Distance.setX(100);
-
-                tv_Time.setY(150 * i + 50);
-                tv_Time.setX(100);
-
-                btn_Choose.setY(150 * i);
-                btn_Choose.setX(800);
-                btn_Remove.setY(150 * i);
-                btn_Remove.setX(500);
-
-                btn_Remove.setBackgroundColor(Color.RED);
-
-                btn_Remove.setOnClickListener(this::OnClickDelete);
                 btn_Choose.setOnClickListener(this::OnClick);
+                btn_Delete.setOnClickListener(this::OnClickDelete);
 
-                relativeLayout.addView(btn_Remove);
+                btn_Delete.setBackgroundColor(Color.RED);
+                btn_Choose.setBackgroundColor(Color.GREEN);
+
+
+                TextView tv_ID = (TextView) T_LL.getChildAt(0);
+                TextView tv_Distance = (TextView)T_LL.getChildAt(1);
+                TextView tv_Timing = (TextView)T_LL.getChildAt(2);
+                ImageView iv_Downloaded = (ImageView)T_LL.getChildAt(3);
+
+                tv_Distance.setX(150);
+                tv_Timing.setX(250);
+                iv_Downloaded.setX(300);
+
+
+                tv_Distance.setTextSize(40);
+                tv_Timing.setTextSize(40);
+                tv_ID.setTextSize(40);
+
+                tv_ID.setText((String)(IDs.get(i)+""));
+                tv_Distance.setText((String)(String.format("%.1f", Distances.get(i))+" м"));
+                tv_Timing.setText(time);
+
+                btn_Choose.setTag(i);
+                btn_Delete.setTag(i);
+
+                linearLayout.addView(T_LL);
+                linearLayout.addView(B_LL);
             }
             else
             {
-                tv_ID.setText((String) (""));
-                tv_Distance.setText("");
-                tv_Time.setText(" ");
-                btn_Choose.setText("Тест");
-
-                tv_ID.setId(5 * i);
-                tv_Distance.setId(5 * i + 1);
-                tv_Time.setId(5 * i + 2);
-                btn_Choose.setId(5 * i + 3);
-
-                tv_ID.setTextSize(30);
-
-
-                tv_ID.setY(150 * i);
-                tv_ID.setX(0);
-
-                tv_Distance.setY(150 * i);
-                tv_Distance.setX(100);
-
-                tv_Time.setY(150 * i + 50);
-                tv_Time.setX(100);
-
-                btn_Choose.setY(150 * i);
-                btn_Choose.setX(800);
-
-                btn_Choose.setOnClickListener(this::OnClickTest);
+                Button btn_RunTest = new Button(this);
+                btn_RunTest.setOnClickListener(this::OnClickTest);
+                linearLayout.addView(btn_RunTest);
             }
 
-            relativeLayout.addView(tv_ID);
-            relativeLayout.addView(tv_Distance);
-            relativeLayout.addView(tv_Time);
-            relativeLayout.addView(btn_Choose);
 
 
         }
